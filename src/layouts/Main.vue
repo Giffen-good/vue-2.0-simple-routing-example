c37b58dcd1ef32ff9246944aecf68e1830a9d845
giffen-good <tal@REDACTED.org>
2019-12-07 12:31:06 -0500

    v1.11

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -5,7 +5,16 @@
 </template>
 
 <script>
-
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
   export default {
     components: {
     }
@@ -26,10 +35,17 @@
   src:url('/public/fonts/Neuzeit-Grotesk-Regular.ttf')
 
 }
-
+html,
+body {
+    overflow: hidden;
+    position: relative;
+    height: 100%;
+}
 body {
     margin:0;
-    overflow: hidden;
+    max-height:100vh;
+    max-width: 100vw;
+    height: calc(var(--vh, 1vh) * 100);
   }
  #main {
     display:flex;
